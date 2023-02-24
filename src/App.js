4ab0d7d5ca1881e89c818e9f75c4ca525c2feca5
OlesiaPastushenko <olesya.pastushenko1@gmail.com>
2023-02-24 13:36:59 +0100

    routing added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom"
+import CheckTTN from './pages/CheckTTN/CheckTTN';
+import List from './pages/List/List';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+    <Route path="/" element={<CheckTTN/>}/>
+    <Route path="/list" element={<List/>}/>
+    <Route path="*" element={<ErrorPage/>}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/CheckTTN/CheckTTN.jsx
@@ -0,0 +1,7 @@
+import {Link} from 'react-router-dom'
+export default function CheckTTN () {
+    return(
+        <div>check
+        </div>
+    )
+}
