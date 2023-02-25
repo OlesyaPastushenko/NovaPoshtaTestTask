c606310d8b1fb582d1cd471982f33e8abc5ee7c7
OlesiaPastushenko <olesya.pastushenko1@gmail.com>
2023-02-25 17:54:14 +0100

    pagination added

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react"
 import axios from "axios";
 import env from "react-dotenv";
 import Card from "../../components/card/Card";
+import './list.css'
 
 export default function List () {
     const [error, setError] = useState('')
     const [loading, setLoading] = useState('Loading...')
     const [results, setResults] = useState([])
+    const [count, setCount] = useState(1)
+    const onPrev = () => {
+        count > 1 ? setCount(prev=>prev-1) : setCount(1)
+    }
+    const onNext = () => {
+        count < 20 ? setCount(prev=>prev+1) : setCount(20)
+    }
     const fetchAdress = async()=>{
         await axios
         .post("https://api.novaposhta.ua/v2.0/json/", {
@@ -15,7 +23,7 @@ export default function List () {
           "calledMethod": "getWarehouses",
           "methodProperties": {
             "CityName" : "Київ",
-            "Page" : "1",
+            "Page" : count,
             "Limit" : "20",
             "Language" : "UA"
           },
@@ -35,12 +43,21 @@ export default function List () {
     useEffect(()=>{
         fetchAdress()
     },[])
-
+    useEffect(()=>{
+        setResults([])
+        fetchAdress()
+        window.scrollTo(0, 0)
+    },[count])
     return(
         <div className="cover">
           <div className="h3">Список відделнь у Києві</div>
-          {results.length < 1 ? <p>{loading}</p> :<Card results={results}/>}
+          {results.length < 1 ? <p className="loading">{loading}</p> :<Card results={results}/>}
           { error && <div>{error}</div>}
+          <div className="page">
+            <button className="pageBtn" onClick={onPrev}>&#60;</button>
+            <div>{count}</div>
+            <button className="pageBtn" onClick={onNext}>&#62;</button>
+          </div>
         </div>
     )
 }
